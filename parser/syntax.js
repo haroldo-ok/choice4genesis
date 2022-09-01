11de5efafb03ff7c502ca3714f1bea015133a85d
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-01 19:54:35 -0300

    Start parser implementation.

---
+++ parser/syntax.js
@@ -0,0 +1,3 @@
+const parse = () => '';
+
+module.exports = { parse };
--- parser/syntax.test.js
+++ parser/syntax.test.js
@@ -1,3 +1,5 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const { parse } = require('./syntax');
+
+test('parse something', () => {
+  expect(parse('')).toBe('');
 });
