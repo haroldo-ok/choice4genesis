f7efdc5f47c2eddda9b2ff6f190a1c2c028dc7d2
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-01 20:15:28 -0300

    Implement parsing of simple lines.

--- parser/syntax.js
+++ parser/syntax.js
@@ -1,3 +1,13 @@
-const parse = () => '';
+const parse = source => {
+	const lines = source.split(/\r?\n/g);
+	
+	return {
+		type: 'script',
+		body: lines.map(line => ({
+			type: 'text',
+			content: line
+		}))
+	};
+};
 
 module.exports = { parse };
--- parser/syntax.test.js
+++ parser/syntax.test.js
@@ -1,5 +1,20 @@
 const { parse } = require('./syntax');
 
-test('parse something', () => {
-  expect(parse('')).toBe('');
+test('parse simple lines', () => {
+const SOURCE = `
+Line 1
+Line 2
+Line 3
+`
+
+	expect(parse(SOURCE)).toEqual({
+		type: 'script',
+		body: [
+			{ type: 'text', content: '' },
+			{ type: 'text', content: 'Line 1' },
+			{ type: 'text', content: 'Line 2' },
+			{ type: 'text', content: 'Line 3' },
+			{ type: 'text', content: '' }
+		]
+	});
 });
