3a7becdc3feede1242ea6364bfad9a1096e75dda
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-02 18:47:09 -0300

    Trim empty lines at start and end.

--- parser/syntax.js
+++ parser/syntax.js
@@ -1,19 +1,39 @@
+const trimBlanks = lines => {
+	let start = 0;
+	let end = lines.length - 1;
+
+	while (start <= end && lines[start].type === 'blank') {
+		start++;
+	}
+	
+	while (end >= start && lines[end].type === 'blank') {
+		end--;
+	}
+	
+	if (start > end) {
+		return [];
+	}
+	return lines.slice(start, end + 1);
+};
+
 const parse = source => {
 	const lines = source.split(/\r?\n/g).map((text, index) => ({
 		text: text.trimEnd(),
 		line: index + 1
 	}));
 	
+	const body = lines.map(({ text, line}) => (text ? {
+		type: 'text',
+		line,
+		text
+	} : {
+		type: 'blank',
+		line
+	}));
+	
 	return {
 		type: 'script',
-		body: lines.map(({ text, line}) => (text ? {
-			type: 'text',
-			line,
-			text
-		} : {
-			type: 'blank',
-			line
-		}))
+		body: trimBlanks(body)
 	};
 };
 
--- parser/syntax.test.js
+++ parser/syntax.test.js
@@ -11,12 +11,10 @@ Line 3
 	expect(parse(SOURCE)).toEqual({
 		type: 'script',
 		body: [
-			{ type: 'blank', line: 1 },
 			{ type: 'text', line: 2, text: 'Line 1' },
 			{ type: 'text', line: 3, text: 'Line 2' },
 			{ type: 'blank', line: 4 },
-			{ type: 'text', line: 5, text: 'Line 3' },
-			{ type: 'blank', line: 6 }
+			{ type: 'text', line: 5, text: 'Line 3' }
 		]
 	});
 });
