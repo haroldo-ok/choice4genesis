9d2635ec48f78a64aec2b656e074b060a0733392
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-04 15:33:12 -0300

    Implement simple math.

--- parser/expression.js
+++ parser/expression.js
@@ -1,3 +1,80 @@
-const parseExpression = (source, config) => null;
+'use strict';
 
-module.exports = { parseExpression };
+const P = require('parsimmon');
+
+// Begin: Utility functions from "math.js", from Parsimmon's demos; see those for full comments
+
+let _ = P.optWhitespace;
+
+const operators = ops => {
+	let keys = Object.keys(ops).sort();
+	let ps = keys.map(k =>
+		P.string(ops[k])
+		.trim(_)
+		.result(k)
+	);
+	return P.alt.apply(null, ps);
+}
+
+const PREFIX = (operatorsParser, nextParser) => {
+	let parser = P.lazy(() => {
+		return P.seq(operatorsParser, parser).or(nextParser);
+	});
+	return parser;
+};
+
+const POSTFIX = (operatorsParser, nextParser) => {
+	return P.seqMap(nextParser, operatorsParser.many(), (x, suffixes) =>
+		suffixes.reduce((acc, x) => [x, acc], x)
+	);
+};
+
+const BINARY_LEFT = (operatorsParser, nextParser) => {
+	return P.seqMap(
+		nextParser,
+		P.seq(operatorsParser, nextParser).many(),
+		(first, rest) => {
+			return rest.reduce((acc, ch) => {
+				let [op, another] = ch;
+				return [op, acc, another];
+			}, first);
+		}
+	);
+}
+
+
+// A simple integer
+const Num = P.regexp(/[0-9]+/)
+	.map(str => ["Number", +str])
+	.desc("number");
+	
+let Expression;
+
+// A basic value is any parenthesized expression or a number.
+const Basic = P.lazy(() =>
+	P.string("(")
+		.then(Expression)
+		.skip(P.string(")"))
+		.or(Num)
+);
+
+// End: Utility functions from "math.js", from Parsimmon's demos
+
+
+
+const table = [
+  { type: PREFIX, ops: operators({ Negate: "-" }) },
+  { type: BINARY_LEFT, ops: operators({ Multiply: "*", Divide: "/" }) },
+  { type: BINARY_LEFT, ops: operators({ Add: "+", Subtract: "-" }) }
+];
+
+const TableParser = table.reduce(
+	(acc, level) => level.type(level.ops, acc),
+	Basic
+);
+
+Expression = TableParser.trim(_);
+
+const createExpressionParser = config => Expression;
+
+module.exports = { createExpressionParser };
--- parser/expression.test.js
+++ parser/expression.test.js
@@ -1,5 +1,14 @@
-const { parseExpression } = require('./expression');
+const { createExpressionParser } = require('./expression');
 
-test('should not crash right away', () => {
-	expect(parseExpression('example', {}));
+
+test('should reject an incorrect value', () => {
+	const result = createExpressionParser({}).parse('?}{/~');
+	expect(result).toMatchObject({ status: false });
+});
+
+test('should perform simple addition', () => {
+	const result = createExpressionParser({}).parse('1 + 3');
+	console.log(result);
+	expect(result).not.toMatchObject({ status: false });
+	expect(result).toEqual({ status: true, value: [ 'Add', [ 'Number', 1 ], [ 'Number', 3 ] ] });
 });
