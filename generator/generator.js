723817185e32fe605008e01c6e3954313a4c2c74
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-11 18:47:10 -0300

    Call the parser.

--- generator/generator.js
+++ generator/generator.js
@@ -1,3 +1,13 @@
-const generate = fileSystem => ({});
+const { parse } = require('../parser/syntax-full');
+
+const generate = fileSystem => {
+	const source = fileSystem.readSource('startup');
+	const ast = parse(source);
+	if (ast.errors) {
+		return { errors: ast.errors };
+	}
+	
+	return {};
+};
 
 module.exports = { generate };
