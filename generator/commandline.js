fd71d86b9b69c78e7e396138d26605beea60369b
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-24 10:08:16 -0300

    Add parameter for setting projects dir.

--- generator/commandline.js
+++ generator/commandline.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const yargs = require('yargs');
+const { normalize } = require('path');
 
 const readCommandLine = () => yargs
 	.command('transpile <project>', 'generates ".c" and ".res" files from the scripts', (yargs) => {
@@ -8,6 +9,14 @@ const readCommandLine = () => yargs
 			describe: 'name of the project to be transpiled',
 			type: 'string'
 		})
+	})
+	.options({
+		'project-dir': {
+			alias: 'pd',
+			default: normalize(__dirname + '/../examples'),
+			normalize: true,
+			type: 'string'
+		}
 	})
 	.demandCommand(1, 'You need at least one command before moving on')
 	.strict()
