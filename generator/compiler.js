b193cfe64b7d092fc7e55324c8477b3567be3e3d
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-25 14:08:01 -0300

    Show compiler logs on the console.

--- generator/compiler.js
+++ generator/compiler.js
@@ -2,26 +2,34 @@
 
 const { existsSync } = require('fs');
 const { normalize } = require('path');
-const { spawnSync } = require('child_process');
+const { spawn } = require('child_process');
 
-const compile = commandLine => {
-	console.log('Calling the compiler!', commandLine);
-	
-	const projectFolder = normalize(`${commandLine.projectDir}/${commandLine.project}/`);
-	if (!existsSync(projectFolder)) {
-		return { errors: [{ message: 'Directory does not exist: ' + projectFolder }] };
-	}
+const compile = async commandLine =>
+	new Promise((resolve, reject) => {
+		const projectFolder = normalize(`${commandLine.projectDir}/${commandLine.project}/`);
+		if (!existsSync(projectFolder)) {
+			resolve({ errors: [{ message: 'Directory does not exist: ' + projectFolder }] });
+		}
 
-	const makeFile = normalize(`${commandLine.sgdkDir}/makefile.gen`);
-	if (!existsSync(makeFile)) {
-		return { errors: [{ message: `Could not find "${makeFile}"; please check your SGDK installation.` }] };
-	}
-	
-	spawnSync(`${commandLine.sgdkDir}/bin/make`, [ makeFile ], {
-		cwd: projectFolder
+		const makeFile = normalize(`${commandLine.sgdkDir}/makefile.gen`);
+		if (!existsSync(makeFile)) {
+			resolve({ errors: [{ message: `Could not find "${makeFile}"; please check your SGDK installation.` }] });
+		}
+		
+		const proc = spawn(`${commandLine.sgdkDir}/bin/make`, [ '-f', makeFile ], {
+			cwd: projectFolder
+		});
+		proc.stdout.on('data', data => console.log(`${data}`.trimRight()))
+		proc.stderr.on('data', data => console.error(`${data}`.trimRight()))
+		proc.on('error', data => console.error(`${data}`.trimRight()))
+		proc.on('close', code => {
+			if (!code) {
+				resolve({});
+			}
+			
+			console.error(`SGDK exited with code ${code}`);
+			resolve({ errors: [{ message: 'SGDK compiler returned an error.' }] });
+		});		
 	});
-	
-	return {};
-};
 
 module.exports = { compile };
