343f1d622b12ba06c595ef6376d1dfdb97b4650e
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-25 13:22:24 -0300

    Call SGDK without console output.

--- generator/compiler.js
+++ generator/compiler.js
@@ -2,14 +2,24 @@
 
 const { existsSync } = require('fs');
 const { normalize } = require('path');
+const { spawnSync } = require('child_process');
 
 const compile = commandLine => {
 	console.log('Calling the compiler!', commandLine);
 	
+	const projectFolder = normalize(`${commandLine.projectDir}/${commandLine.project}/`);
+	if (!existsSync(projectFolder)) {
+		return { errors: [{ message: 'Directory does not exist: ' + projectFolder }] };
+	}
+
 	const makeFile = normalize(`${commandLine.sgdkDir}/makefile.gen`);
 	if (!existsSync(makeFile)) {
 		return { errors: [{ message: `Could not find "${makeFile}"; please check your SGDK installation.` }] };
 	}
+	
+	spawnSync(`${commandLine.sgdkDir}/bin/make`, [ makeFile ], {
+		cwd: projectFolder
+	});
 	
 	return {};
 };
