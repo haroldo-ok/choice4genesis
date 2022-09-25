417759d6920f8a500ada504dc1d1f331379f60f9
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-25 11:27:13 -0300

    Add command line options for SGDK

--- generator/commandline.js
+++ generator/commandline.js
@@ -10,11 +10,25 @@ const readCommandLine = () => yargs
 			type: 'string'
 		})
 	})
+	.command('compile <project>', 'transpiles the project first, then compiles it using SGDK', (yargs) => {
+		yargs.positional('project', {
+			describe: 'name of the project to be transpiled',
+			type: 'string'
+		})
+	})
 	.options({
 		'project-dir': {
 			alias: 'pd',
 			default: normalize(__dirname + '/../examples'),
 			normalize: true,
+			describe: 'Directory where the projects are located',
+			type: 'string'
+		},
+		'sgdk-dir': {
+			alias: 'sd',
+			default: normalize(__dirname + '/../../sgdk170'),
+			normalize: true,
+			describe: 'Directory where SGDK is located',
 			type: 'string'
 		}
 	})
