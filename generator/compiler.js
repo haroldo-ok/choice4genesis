cd0ed5eac398a778ff6038f0bf9087d220dde486
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-09-25 13:13:57 -0300

    Check if the Makefile exists.

--- generator/compiler.js
+++ generator/compiler.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const { existsSync } = require('fs');
+const { normalize } = require('path');
+
 const compile = commandLine => {
 	console.log('Calling the compiler!', commandLine);
+	
+	const makeFile = normalize(`${commandLine.sgdkDir}/makefile.gen`);
+	if (!existsSync(makeFile)) {
+		return { errors: [{ message: `Could not find "${makeFile}"; please check your SGDK installation.` }] };
+	}
+	
 	return {};
 };
 
