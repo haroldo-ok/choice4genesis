2fae6556d54577d10d6bbd522ddcf76b13ba87e7
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-11-06 19:35:51 -0300

    Check if Imagemagick paths exist.

--- generator/image.js
+++ generator/image.js
@@ -1,10 +1,18 @@
 const { identify } = require('imagemagick');
 const { normalize } = require('path');
+const { existsSync } = require('fs');
 
 const convertImages = async (result, projectFolder) => {
 	const imagemagickDir = normalize(__dirname + '/../../ImageMagick-7.0.10-53-portable-Q16-x86');
+	if (!existsSync(imagemagickDir)) {
+		return { errors: [{ message: 'Imagemagick directory does not exist at: ' + projectFolder }] };
+	}
 
 	identify.path = normalize(imagemagickDir + '/identify.exe');
+	if (!existsSync(identify.path)) {
+		return { errors: [{ message: 'Imagemagick "identify" tool not found at: ' + identify.path }] };
+	}
+	
 	identify(normalize(projectFolder + '/project/' + Object.entries(result.images)[0][0]), (err, metadata) => console.log(err, metadata));
 	
 	return result;
