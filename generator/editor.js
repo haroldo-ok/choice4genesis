cf7d5d689906cfd74d6b16a4e57accd563d984bd
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-12 20:48:23 -0300

    Call `parcel` when calling the `edit` command.
    Starting to implement #88

--- generator/editor.js
+++ generator/editor.js
@@ -1,7 +1,22 @@
 'use strict';
 
-const showEditor = (commandLine, executeCommands) => {
-	console.log('OK')
+const { Parcel } = require('@parcel/core');
+
+const showEditor = async (commandLine, executeCommands) => {
+	let bundler = new Parcel({
+		entries: 'editor/index.html',
+		defaultConfig: '@parcel/config-default',
+		serveOptions: {
+			port: 1234
+		},
+		hmrOptions: {
+			port: 1234
+		}
+	});
+
+	await bundler.watch();
+
+	console.log('OK', Parcel);
 };
 
 module.exports = { showEditor };
