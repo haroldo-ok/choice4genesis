4397058ef16e46d9e16eb0581c25dad7435b33d4
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-12 20:55:28 -0300

    Open in browser after starting `parcel`.

--- generator/editor.js
+++ generator/editor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { Parcel } = require('@parcel/core');
+const { openInBrowser } = require('@parcel/utils');
 
 const showEditor = async (commandLine, executeCommands) => {
 	let bundler = new Parcel({
@@ -15,6 +16,8 @@ const showEditor = async (commandLine, executeCommands) => {
 	});
 
 	await bundler.watch();
+	
+	openInBrowser('http://localhost:1234/');
 
 	console.log('OK', Parcel);
 };
