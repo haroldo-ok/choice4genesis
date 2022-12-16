a412a63a92262af30e32211211d4a92bb44b497f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-15 22:07:28 -0300

    Setup routing for project listing.

--- editor/back/backend.js
+++ editor/back/backend.js
@@ -3,11 +3,14 @@
 const express = require('express');
 
 const startBackend = port => {
-	const app = express();
-
-	app.get('/', (req, res) => {
-	  res.send('OK!')
+	const api = express.Router();
+	api.get('/projects', (req, res) => {
+		res.send('OK!')
 	});
+
+	const app = express();
+		
+	app.use('/v0', api);
 	app.listen(port);
 
 	console.log(`Backend running on port ${port}`);
