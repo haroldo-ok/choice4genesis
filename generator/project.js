719e4b66490d59e5f3748499da362c88340aaed1
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-17 09:28:37 -0300

    Start refactoring file listing.

--- generator/project.js
+++ generator/project.js
@@ -7,7 +7,32 @@ const getProjectsFolder = commandLine => normalize(commandLine.projectDir);
 
 const isProjectsFolderPresent = async commandLine => exists(getProjectsFolder(commandLine));
 
+const listProjectFiles = async (commandLine, { directory, filter } = {}) => {
+	directory ||= '/';
+	filter ||= file => true;
+	console.log({ directory, filter });
+	
+	//const projectsFolder = getProjectsFolder(commandLine);
+	const projectsFolder = getProjectsFolder(commandLine);
+	const baseDir = normalize(`${projectsFolder}/${directory}`);
+	
+	const fileNames = await readdir(baseDir);
+	console.log({ fileNames, baseDir });
+	const fileNamesStat = await Promise.all(fileNames.map(async fileName => ({
+		fileName, 
+		isDirectory: (await lstat(normalize(`${baseDir}/${fileName}`))).isDirectory()
+	})));
+	
+	return fileNamesStat.filter(filter);
+	console.log({ fileNames, fileNamesStat });
+};
+
 const listProjectNames = async commandLine => {
+	/*
+	await listProjectFiles(commandLine);
+	return;
+	*/
+	
 	const projectsFolder = getProjectsFolder(commandLine);
 	const fileNames = await readdir(getProjectsFolder(commandLine));
 	const fileNamesStat = await Promise.all(fileNames.map(async fileName => ({
