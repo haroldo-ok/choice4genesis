b4f07ecc2c45b121368a49e9abe7035f4dba54de
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-18 18:07:28 -0300

    Replace `useFetch()` with a custom implementation.
    The implementation used by `usehooks-ts` is hardcoded to always use
    JSON.

--- editor/front/hooks.js
+++ editor/front/hooks.js
@@ -1,6 +1,39 @@
-import { useFetch } from 'usehooks-ts';
+'use strict';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const BASE_URL = '/api/v0';
+
+// Based on https://dev.to/techcheck/custom-react-hook-usefetch-eid
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+      setLoading('loading...')
+      setData(null);
+      setError(null);
+      const source = axios.CancelToken.source();
+      axios.get(url, { cancelToken: source.token })
+      .then(res => {
+          setLoading(false);
+          //checking for multiple responses for more flexibility 
+          //with the url we send in.
+          res.data && setData(res.data);
+          res.content && setData(res.content);
+      })
+      .catch(err => {
+          setLoading(false)
+          setError('An error occurred. Awkward..')
+      })
+      return () => {
+          source.cancel();
+      }
+  }, [url]);
+
+  return { data, loading, error };
+}
 
 export function useProjectListApi() {
 	return useFetch(`${BASE_URL}/projects`);
