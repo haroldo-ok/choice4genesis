b8f1601ab39901bd2c56286733f6a0edf935f16f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-19 19:03:51 -0300

    Move scene list to the side.

--- editor/front/App.js
+++ editor/front/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
 import '@picocss/pico/css/pico.min.css'
+import './App.css'
+
 import { prepareSyntax } from './syntax'
 import { ProjectList } from './ProjectList'
 import { SceneList } from './SceneList'
@@ -15,9 +17,16 @@ export function App() {
 
 	return <div>
 		<ProjectList value={projectName} onChange={setProjectName} />
-		<article>
-			<SceneList projectName={projectName} value={sceneName} onChange={setSceneName} />
-		</article>
-		<Scene projectName={projectName} sceneName={sceneName} />
+		<div className="editContainer">
+			<nav>
+				<article>
+					<header>Scenes</header>
+					<SceneList projectName={projectName} value={sceneName} onChange={setSceneName} />
+				</article>
+			</nav>
+			<div>
+				<Scene projectName={projectName} sceneName={sceneName} />
+			</div>
+		</div>
 	</div>
 }
