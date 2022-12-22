fbc4835e0014d282fdb94594431202590f0904b4
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-12-22 18:24:10 -0300

    Try to handle scene changes.

--- editor/front/Scene.js
+++ editor/front/Scene.js
@@ -1,26 +1,37 @@
 'use strict';
 
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+
 import { useSceneApi } from './hooks/api';
+import { useScene } from './hooks/scene';
 
 export function Scene(props) {
 	if (!props.projectName) return <h1>Please, select a project...</h1>;
 	if (!props.sceneName) return <h1>Please, select a scene...</h1>;
 	
 	const { data, error } = useSceneApi(props.projectName, props.sceneName);
+	const { isModified, setData } = useScene(props.sceneName, data);
+	
+	const handleEditorChange = newData => {
+		setData(newData);
+	};
 	
 	if (error) {
 		console.error(`Error while loading scene ${props.sceneName}`, error);
 		return <h1>Error while loading scene {props.sceneName}</h1>;
-	}
-	if (!data) return <h1>Loading scene "{props.sceneName}"...</h1>;
+	}			
+	if (!data) return <h1>Loading scene "{props.sceneName}"...</h1>;	
 	
 	return (
-	   <Editor
-		 height="90vh"
-		 defaultLanguage="choicescript"
-		 path={`${props.projectName}/project/${props.sceneName}`}
-		 defaultValue={data}
-	   />
+		<div>
+		   <h1>{ isModified }</h1>
+		   <Editor
+			 height="90vh"
+			 defaultLanguage="choicescript"
+			 path={`${props.projectName}/project/${props.sceneName}`}
+			 defaultValue={data}
+			 onChange={handleEditorChange}
+		   />
+		</div>
    );
 };
