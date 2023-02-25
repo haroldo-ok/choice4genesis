9a17f03ee2d2bc1fab0a2c154f7a9da10d962bda
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-02-25 17:47:35 -0300

    Serve the static files.
    API proxy is not working.

--- editor/editor.js
+++ editor/editor.js
@@ -4,7 +4,28 @@ const PARCEL_PORT = 1234;
 const API_PORT = 1235;
 
 const showEditor = async (commandLine, executeCommands) => {
-	throw new Error('Not supported.');
+	const { normalize } = require('path');
+	const { openInBrowser } = require('@parcel/utils');
+	
+	const express = require('express');
+	const url = require('url');
+	const proxy = require('express-http-proxy');
+	
+	const { startBackend } = require('./back/backend');
+	
+	startBackend(commandLine, API_PORT);	
+	
+	const app = express();
+	app.use(express.static(normalize(__dirname + '/front/dist')));
+	
+	const apiProxy = proxy('http://localhost:' + API_PORT + '/', {
+		proxyReqPathResolver: req => url.parse(req.baseUrl).path
+	});	
+	app.use('/api/*', apiProxy);
+	
+	app.listen(PARCEL_PORT);
+
+	
 	/*
 	const { Parcel } = require('@parcel/core');
 	const { openInBrowser } = require('@parcel/utils');
@@ -27,12 +48,12 @@ const showEditor = async (commandLine, executeCommands) => {
 	});
 
 	await bundler.watch();	
+	*/
 	console.log(`Frontend running on port ${PARCEL_PORT}`);
 
 	if (commandLine.openBrowser) {
 		openInBrowser(`http://localhost:${PARCEL_PORT}/`);
 	}
-	*/
 };
 
 module.exports = { showEditor };
